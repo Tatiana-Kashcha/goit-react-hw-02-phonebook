e0b3480ad09ab1831c6d35135f7800087cadf1a4
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-12 21:12:45 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,6 +5,53 @@ class ContactForm extends Component {
     name: '',
     number: '',
   };
+
+  handleCange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // this.props.addUser({ ...this.state });
+
+    // this.props.closeForm();
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. 
+            For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={this.handleCange}
+          />
+        </label>
+
+        <label>
+          Email
+          <input
+            type="email"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={this.handleCange}
+          />
+        </label>
+
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
 }
 
 export default ContactForm;
